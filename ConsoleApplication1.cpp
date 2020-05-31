3234e1d5a5f0613c6496d7584c43ed4b17345ea4
sander <cheapcode@yandex.ru>
2020-05-31 18:21:40 +0300

    first commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include<ctime>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    int yearMatrix[365];
+    int twoDigitSum=0;
+    for (int i = 0; i < 365; i++)
+    {
+        yearMatrix[i] = 1+rand() % 100;
+        if(yearMatrix[i]>9 && yearMatrix[i] < 100)
+            twoDigitSum ++;
+    }
+    cout << twoDigitSum;
+}
+
